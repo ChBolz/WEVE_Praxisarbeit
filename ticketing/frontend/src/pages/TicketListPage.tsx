aaf7263acb9971b8b896653cb3e0af74bbd4e7d0
Christoph Bolz <Christoph Bolz@LAPTOP-2CQ14T6U>
2026-02-15 17:33:38 +0100

    Automatically refresh ticket list

--- ticketing/frontend/src/pages/TicketListPage.tsx
+++ ticketing/frontend/src/pages/TicketListPage.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 import type { Ticket, TicketStatus } from "../types/ticket";
 import TicketList from "../components/TicketList";
 import { getTickets } from "../services/ticketAPI";
+import { useLocation } from "react-router-dom";
 
 export default function TicketListPage() {
   const [tickets, setTickets] = useState<Ticket[]>([]);
   const [filter, setFilter] = useState<TicketStatus | "all">("all");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const location = useLocation();
 
   useEffect(() => {
     async function load() {
       try {
+        setLoading(true);
+        setError(null);
         const data = await getTickets();
         setTickets(data);
       } catch {
@@ -22,7 +26,7 @@ export default function TicketListPage() {
     }
 
     load();
-  }, []);
+  }, [location.key]);
 
   if (loading) return <p>Lade Tickets...</p>;
   if (error) return <p>{error}</p>;
